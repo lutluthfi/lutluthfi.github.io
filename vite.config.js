c6e9abc685e3b98a3b6bbaccc80beb982ba4be7b
Arif Luthfiansyah <lutluthfiansyah@gmail.com>
2026-06-30 07:16:05 +0700

    chore: update base config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "https://lutluthfi.github.io/",
+  base: "/lutluthfi.github.io/",
   plugins: [react(), tailwindcss()],
 });
